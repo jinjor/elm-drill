f62b0678ff4238ce1fbc22866851baaf647c75ba
jinjor <jinjorweb@gmail.com>
2017-11-30 10:47:10 +0900

    fix T5 comparison

--- tests/T5_Records.elm
+++ tests/T5_Records.elm
@@ -11,14 +11,14 @@ import TestUtil exposing (testQA, testQA2, testQA3)
 suite : Test
 suite =
     describe "Records"
-        [ testQA "getName" person A.getName A.getName
-        , testQA "getName2" person A.getName2 A.getName2
-        , testQA "getAge" person A.getAge A.getAge
-        , testQA2 "makePerson" string int A.makePerson A.makePerson
-        , testQA2 "makePerson2" string int A.makePerson2 A.makePerson2
-        , testQA "incrementAge" person A.incrementAge A.incrementAge
-        , testQA "isValid" person A.isValid A.isValid
-        , testQA "getRole" roled A.getRole A.getRole
+        [ testQA "getName" person Q.getName A.getName
+        , testQA "getName2" person Q.getName2 A.getName2
+        , testQA "getAge" person Q.getAge A.getAge
+        , testQA2 "makePerson" string int Q.makePerson A.makePerson
+        , testQA2 "makePerson2" string int Q.makePerson2 A.makePerson2
+        , testQA "incrementAge" person Q.incrementAge A.incrementAge
+        , testQA "isValid" person Q.isValid A.isValid
+        , testQA "getRole" roled Q.getRole A.getRole
         ]
 
 
